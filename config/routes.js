4891799fbe770165aacbf5d7240ea9616fd6adb5
Rotteror <xeziod@gmail.com>
2021-06-12 18:35:14 +0300

    Implement Sticker func

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,11 @@
 const { about } = require('../controllers/about');
 const { catalog } = require('../controllers/catalog');
 const { create, post } = require('../controllers/create');
-const { details } = require('../controllers/details');
+const { details, attach, attachPost } = require('../controllers/details');
 const { notFound } = require('../controllers/notFound');
 const { edit, post: editPost } = require('../controllers/edit');
 const { post: commentPost } = require('../controllers/comments')
+const { createAccessory, accessoryPost } = require('../controllers/accessory')
 
 module.exports = (app) => {
 
@@ -16,6 +17,10 @@ module.exports = (app) => {
     app.get('/edit/:id', edit)
     app.post('/edit/:id', editPost)
     app.post('/comments/:cubeId/create', commentPost)
+    app.get('/accessory/create', createAccessory)
+    app.post('/accessory/create', accessoryPost)
+    app.get('/details/:id/attach', attach)
+    app.post('/details/:cubeId/attach', attachPost)
 
     app.all('*', notFound)
 }
